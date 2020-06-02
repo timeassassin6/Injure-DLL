92e9c44e2ad84d50c7bb5f6aa8b846d90d7fa90c
李昊东 <lhd_wyyx@163.com>
2020-06-02 23:21:49 +0800

    20200602

--- Injure DLL/SetContextInject.cpp
+++ Injure DLL/SetContextInject.cpp
@@ -7,12 +7,34 @@ struct _DATA_SHELLCODE //声明存放shellcode的结构体
 	BYTE shellcode[0x30];
 	ULONG_PTR addrofLoadlibraryA;
 	PBYTE lpdllpath;
-	ULONG ori_eip;
+	ULONG ori_rip;
 }DATA_SHELLCODE;
 
 BOOL InjectDllToProcessBySetContext(SIZE_T pid)
 {
+	SIZE_T count = 0;
 	SIZE_T TidTable[MAX_PATH];
-	CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, NULL);
+	THREADENTRY32 ThreadEntry;
+	HANDLE hThread;//存储临时线程句柄
+	CONTEXT context;//存储临时线程CONTEXT
+	HANDLE hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
+	if (Thread32First(hThreadSnap, &ThreadEntry))
+	{
+		do
+		{
+			if (ThreadEntry.th32OwnerProcessID == pid)
+			{
+				TidTable[count++] = ThreadEntry.th32ThreadID;
+			}
+
+		} while (Thread32Next(hThreadSnap, &ThreadEntry));
+	}
+	for (count; count > 0; count--)
+	{
+		hThread = OpenThread(THREAD_ALL_ACCESS, NULL, TidTable[count]);
+		GetThreadContext(hThread, &context);
+		context.rip
+	}
+
 
 }
